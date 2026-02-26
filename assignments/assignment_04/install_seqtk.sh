97131b40651b2011530ca4b30bc71061d2f8d524
Paulina Skurzak <pskurzak@wm.edu>
2026-02-26 00:34:34 -0500

    Add install scripts to assignment 04

--- assignments/assignment_04/install_seqtk.sh
+++ assignments/assignment_04/install_seqtk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+set -ueo pipefail
 cd ~/programs
-
+# clone seqtk
 git clone https://github.com/lh3/seqtk.git
-
+# compile
 cd seqtk
-
 make
-
+# add seqtk to PATH
 echo "export PATH=$PATH:~/programs/seqtk" >> ~/.bashrc
+echo "seqtk installed in ~/programs/seqtk"
