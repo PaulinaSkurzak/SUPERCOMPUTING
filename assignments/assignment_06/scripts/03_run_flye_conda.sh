ebdb9b45717230e4512b0039cd54fe41318e7539
Paulina Skurzak <pskurzak@wm.edu>
2026-03-19 02:24:40 -0400

    Fix conda script and pipeline cleanup

--- assignments/assignment_06/scripts/03_run_flye_conda.sh
+++ assignments/assignment_06/scripts/03_run_flye_conda.sh
@@ -8,12 +8,12 @@ mkdir -p ./assemblies/assembly_conda
 
 flye --nano-hq ./data/SRR33939694.fastq.gz --meta --threads 6 --out-dir ./assemblies/assembly_conda
 
-mv ./assemblies/assembly_conda/assembly.fasta ./assemblies/assembly_conda/conda_assembly.fasta
-mv ./assemblies/assembly_conda/flye.log ./assemblies/assembly_conda/conda_flye.log
+mv ./assemblies/assembly_conda/assembly.fasta ./conda_assembly.fasta
+mv ./assemblies/assembly_conda/flye.log ./conda_flye.log
 
-rm -f ./assemblies/assembly_conda/assembly_graph.gfa
-rm -f ./assemblies/assembly_conda/assembly_graph.gv
-rm -f ./assemblies/assembly_conda/assembly_info.txt
-rm -f ./assemblies/assembly_conda/params.json
+rm -rf ./assemblies/assembly_conda/*
+
+mv ./conda_assembly.fasta ./assemblies/assembly_conda/
+mv ./conda_flye.log ./assemblies/assembly_conda/
 
 conda deactivate
